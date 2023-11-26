64bce6a308c87a18ab1fbd7d3753bab8c477568f
Thiago Maia <thiago.maia971@gmail.com>
2023-11-26 16:48:33 -0300

    publish

--- search.sh
+++ search.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 version=$1
-versionText="  version=${version}"
+versionText="  version: ${version}"
 mylist=$(find ./ -type f -wholename "./plugins/*/plugin.yaml")
 
 for file in $mylist; do
     echo $file
     sed -i "7s/.*/${versionText}/" $file
+    (cd ${file%/*}; echo $(pwd); stk publish plugin -s thiagomaia971)
 done
